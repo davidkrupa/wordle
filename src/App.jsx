3316387bd764905526a5827e3bed5571e55644e3
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-07-12 20:14:56 +0200

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,71 @@ const ROWS_NUMBER = 6;
 const WORD_LENGTH = 5;
 
 function App() {
-  const [secretWord, setSecretWord] = useState("PRICE");
+  const [secretWord, setSecretWord] = useState("price");
   const [chosenWords, setChosenWords] = useState(Array(ROWS_NUMBER).fill(null));
   const [currentWord, setCurrentWord] = useState(Array(WORD_LENGTH).fill(null));
-  const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+  const [nextWordIndex, setNextWordIndex] = useState(0);
+  const [nextLetterIndex, setNextLetterIndex] = useState(0);
+  const [isWordApproved, setIsWordApproved] = useState(false);
 
+  const isWon = chosenWords.includes(secretWord);
   const regex = /^[A-Za-z]+$/;
 
   useEffect(() => {
     const handleKeyPress = (e) => {
+      if (isWon) return;
+
+      if (chosenWords[ROWS_NUMBER - 1] !== null) return; // when last word in the board approved prevent other actions
+
+      if (e.key === "Enter" && currentWord[WORD_LENGTH - 1] !== null) {
+        if (isWordApproved) return; // prevent updating states when Enter clicked multiple times
+        setIsWordApproved(true);
+        setNextLetterIndex(0);
+        setNextWordIndex(nextWordIndex + 1);
+        setCurrentWord((prev) => prev.map(() => null));
+        setChosenWords((prev) =>
+          prev.map((item, index) => {
+            if (nextWordIndex !== index) return item;
+            else return currentWord.join("");
+          })
+        );
+        return;
+      }
+
+      // update state to default value when new word is typed
+      if (isWordApproved) {
+        setIsWordApproved(false);
+      }
+
+      if (e.key === "Backspace" && !isWordApproved) {
+        if (nextLetterIndex === 0) return; // prevent updating states when Backspace clicked and line is empty
+        setCurrentWord((prev) => {
+          setNextLetterIndex(nextLetterIndex - 1);
+
+          return prev.map((item, index) => {
+            if (nextLetterIndex - 1 === index) return null;
+            else return item;
+          });
+        });
+        return;
+      }
+
+      // when there is letter in the last place in the line prevent other actions
+      if (currentWord[WORD_LENGTH - 1] !== null && !isWordApproved) return;
+
       // checks if only letters are typed
       if (!e.key.match(regex) || e.key.length !== 1) return;
 
-      const updatedCurrentWord = currentWord.map((item, index) => {
-        if (index !== currentLetterIndex) return item;
-        else return e.key;
-      });
+      setCurrentWord((prev) =>
+        prev.map((item, index) => {
+          if (index !== nextLetterIndex) return item;
+          else return e.key.toLowerCase();
+        })
+      );
 
-      setCurrentWord((prev) => {
-        if (currentLetterIndex === WORD_LENGTH - 1) {
-          setCurrentWordIndex(currentWordIndex + 1);
-          setChosenWords((prevWord) =>
-            prevWord.map((item, index) => {
-              if (index !== currentWordIndex) return item;
-              else return updatedCurrentWord.join("");
-            })
-          );
-          return prev.map((el) => null);
-        } else return updatedCurrentWord;
-      });
-
-      setCurrentLetterIndex((prev) => {
-        if (prev === WORD_LENGTH - 1) return 0;
-        else return prev + 1;
+      setNextLetterIndex((prev) => {
+        if (prev !== WORD_LENGTH) return prev + 1;
+        else return prev;
       });
     };
 
@@ -47,19 +78,33 @@ function App() {
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  }, [currentWord]);
+  }, [currentWord, isWordApproved, nextLetterIndex]);
+
+  const handleClick = () => {
+    setChosenWords((prev) => prev.map(() => null));
+    setCurrentWord((prev) => prev.map(() => null));
+    setNextWordIndex(0);
+    setNextLetterIndex(0);
+    setIsWordApproved(false);
+  };
 
   return (
     <main className="site-wrapper">
-      <h1>Wordle</h1>
+      <h1>{!isWon ? "Wordle" : "You won!"}</h1>
       <div className="board">
         {chosenWords.map((item, index) => (
           <WordLine
             key={index}
-            word={currentWordIndex === index ? currentWord : item}
+            word={nextWordIndex === index ? currentWord : item}
+            secretWord={secretWord}
+            isCurrent={index === nextWordIndex}
           />
         ))}
       </div>
+      {(chosenWords[ROWS_NUMBER - 1] !== null && (
+        <button onClick={() => handleClick()}>Play Again</button>
+      )) ||
+        (isWon && <button onClick={() => handleClick()}>Play Again</button>)}
     </main>
   );
 }
--- src/WordLine.jsx
+++ src/WordLine.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 
-const WordLine = ({ word }) => {
+const WordLine = ({ word, secretWord, isCurrent }) => {
   let letters;
 
   if (word === null) {
-    letters = new Array(5).fill("");
+    letters = new Array(5).fill(""); // when use chosenWords and element is null
   } else if (typeof word === "string") {
-    letters = word.split("");
-  } else letters = word;
+    letters = word.split(""); // when use chosenWords and element is string
+  } else letters = word; // when use currentWord array which contains letters
+
+  const boxStyle = letters.map((item, index) => {
+    if (item === "") return "";
+    if (item === secretWord[index]) return "correct";
+    if (secretWord.includes(item)) return "close";
+    else return "incorrect";
+  });
 
   return (
     <div className="word-line">
       {letters.map((item, index) => (
-        <div className="letter-box" key={index}>
-          {item}
+        <div
+          className={`letter-box ${!isCurrent ? boxStyle[index] : ""}`}
+          key={index}
+        >
+          {item === null ? item : item.toUpperCase()}
         </div>
       ))}
     </div>
